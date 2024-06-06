f3e8a19b96ee9c9cf3cee991edf6e87e1883dbb0
Franco <francomurabito@outlook.com>
2024-06-06 20:17:22 -0300

    init project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
+
+async function bootstrap() {
+  const logger = new Logger('Payments-MS');
+  const app = await NestFactory.create(AppModule);
+  await app.listen(envs.port);
+  logger.log(`Payments-MS is running on port ${envs.port}`);
+}
+bootstrap();
